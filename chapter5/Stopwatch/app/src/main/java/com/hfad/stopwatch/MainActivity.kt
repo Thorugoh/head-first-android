fc675794384c96f30ceca38f5c3ccc6e68e77235
Victor Hugo <pavictorhugo@gmail.com>
2022-10-17 22:38:12 -0300

    (chapter5): Add actions to buttons

--- chapter5/Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ chapter5/Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -2,10 +2,52 @@ package com.hfad.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            if(!running){
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if(running){
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    fun setBaseTime(){
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset(){
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
     }
 }
