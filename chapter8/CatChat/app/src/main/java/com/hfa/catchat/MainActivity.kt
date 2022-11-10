bf2d4b15d3b6a9f003ef4474d4e53e00cd6f3cb8
Victor Hugo <pavictorhugo@gmail.com>
2022-11-09 22:08:18 -0300

    (chapter8): Create CatChat App

---
+++ chapter8/CatChat/app/src/main/java/com/hfa/catchat/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hfa.catchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
