9ec466bb2e86663ac60e2db2e0ead9f55a0cf18c
Victor Hugo <pavictorhugo@gmail.com>
2022-11-09 22:38:27 -0300

    (chapter8): Add toolbar actions

--- chapter8/CatChat/app/src/main/java/com/hfa/catchat/MainActivity.kt
+++ chapter8/CatChat/app/src/main/java/com/hfa/catchat/MainActivity.kt
@@ -2,6 +2,10 @@ package com.hfa.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
